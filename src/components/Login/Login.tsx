3483b1fa0d29d421a81b67d0eb8c48860fca3666
Vika <Vikasatta1998@gmail.com>
2021-09-28 00:27:43 +0300

    75

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 const Login = () => {
     return (
+        <div>
         <h1>Login</h1>
+        <LoginForm/>
+        </div>
+    );
+};
+
+const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'login'}/>
+            </div>
+            <div>
+                <input placeholder={'password'}/>
+            </div>
+            <div>
+                <input type={'checkbox'}/> remember me
+            </div>
+            <div>
+                <button>
+                    Login
+                </button>
+            </div>
+        </form>
     );
 };
 
