8d345b2fd7a77fbcd1cd71da09b249ca001f6328
Vika <Vikasatta1998@gmail.com>
2022-01-18 17:31:54 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-        <BrowserRouter>
+        <BrowserRouter >
             <Provider store={store}>
             <App />
             </Provider>
